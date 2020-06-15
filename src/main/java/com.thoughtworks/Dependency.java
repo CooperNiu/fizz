703f5df7d3f54ce4d2182488db1b7f2af5c9bfcb
Niuyan <niuyanno.1@outlook.com>
2020-06-15 15:39:50 +0800

    first commit

---
+++ src/main/java/com.thoughtworks/Dependency.java
@@ -0,0 +1,7 @@
+package com.thoughtworks;
+
+public class Dependency {
+    public String say(){
+        return "Leave me alone.";
+    }
+}
---
+++ src/main/java/com.thoughtworks/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.thoughtworks;
+
+public class HelloWorld {
+    private Dependency dependency;
+
+    public HelloWorld(Dependency dependency) {
+
+        this.dependency = dependency;
+    }
+    public String beenCalled() {
+        return dependency.say();
+    }
+}
